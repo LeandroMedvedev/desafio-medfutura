0cabbb47eb6b8dce1833dd5872b9aec521378cf2
LeandroMedvedev <medvedevleandro@outlook.com>
2024-09-01 23:28:58 -0300

    feat: middleware implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const registrador = require('./middlewares/registrador');
+const tratamentoErros = require('./middlewares/tratamentoErros');
 const pessoaRoutes = require('./routes/pessoaRoutes');
 const Pessoa = require('./models/Pessoa');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(pessoaRoutes);
+app.use(registrador); // Middleware para registrar as requisições
+app.use('/api', pessoaRoutes);
+app.use(tratamentoErros); // Middleware para tratamento de erros
 
-Pessoa.criarTabela();
+Pessoa.criarTabela(); // Criar a tabela 'Pessoas' na base de dados, se não existir
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Aplicação rodando em http://localhost:${port}.`);
--- src/routes/pessoaRoutes.js
+++ src/routes/pessoaRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const PessoaController = require('../controllers/PessoaController');
+const validarPessoa = require('../middlewares/validarPessoa');
+const verificarId = require('../middlewares/verificarId');
 const router = express.Router();
 
-router.post('/pessoas', PessoaController.criarPessoa);
-router.get('/pessoas/:id', PessoaController.buscarPessoa);
+router.post('/pessoas', validarPessoa, PessoaController.criarPessoa);
+router.get('/pessoas/:id', verificarId, PessoaController.buscarPessoa);
 router.get('/pessoas', PessoaController.buscarPessoas);
-router.put('/pessoas/:id', PessoaController.atualizarPessoa);
-router.delete('/pessoas/:id', PessoaController.excluirPessoa);
+router.put(
+  '/pessoas/:id',
+  verificarId,
+  validarPessoa,
+  PessoaController.atualizarPessoa
+);
+router.delete('/pessoas/:id', verificarId, PessoaController.excluirPessoa);
 
 module.exports = router;
